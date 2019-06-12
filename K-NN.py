ec6a576d32621976bf352d2916977c650dd090fd
gabrields1998 <gabrieldavidsacca@hotmail.com>
2019-06-11 21:19:27 -0300

    atualização K-NN funcional

--- K-NN.py
+++ K-NN.py
@@ -9,7 +9,8 @@ Teste = arquivoTeste.read().split('\n')
 for i in range(len(Treino) -1):
 	Treino[i] = Treino[i].split(' ')
 
-for i in range(len(Teste) - 1): # itera por todos os testes
+acertou = 0
+for i in range(len(Teste) -1): # itera por todos os testes
 	Teste[i] = Teste[i].split(' ')
 	# print(float(Teste[i][0]))
 	distanciaEuclidiana = []
@@ -21,21 +22,25 @@ for i in range(len(Teste) - 1): # itera por todos os testes
 			if((len(Treino[j]) -1) > k):
 				result += (float(Teste[i][k]) - float(Treino[j][k]))**2
 			else:
-				print('treino = ', Treino[j][k])
 				classeTreino = int(Treino[j][k])
+
 		sqrt = result ** (0.5)
+
 		distanciaEuclidiana.append([sqrt,classeTreino])
 
-
-	print(distanciaEuclidiana[0])
 	for m in range(len(distanciaEuclidiana) - 1 ): # ordena lista euclidiana
 		for n in range(len(distanciaEuclidiana) - 1 ):
 			if (distanciaEuclidiana[m][0] < distanciaEuclidiana[n][0]):
-				aux = distanciaEuclidiana[m][0]
-				distanciaEuclidiana[m][0] = distanciaEuclidiana[n][0]
-				distanciaEuclidiana[n][0] = aux
+				aux = distanciaEuclidiana[m]
+				distanciaEuclidiana[m] = distanciaEuclidiana[n]
+				distanciaEuclidiana[n] = aux
+
 
 	if (distanciaEuclidiana[0][1] == int(Teste[i][len(Teste[i]) - 1])):
-		print("acertou classe = ", distanciaEuclidiana[0][1])
-	else:
-		print("errou classe = ", distanciaEuclidiana[0][1])
+		# print("acertou classe = ", distanciaEuclidiana[0][1])
+		acertou += 1
+	# else:
+		# print(int(Teste[i][len(Teste[i]) - 1]))
+		# print("errou classe = ", distanciaEuclidiana[0][1])
+
+print(acertou, "|", len(Teste))
